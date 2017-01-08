f2b02a77c53e6f8b7c3dc0f2972dcbe6e2884c81
gceico <ceicoschi.gabriel@gmail.com>
2017-01-08 13:46:52 +0200

    Actualizare sdl.h
    Am inclus librariile necesare, am schimbat structura "enum color"
    folosind doar culorile necesare, am definit numarul de bits per pixel.

--- Tetris/sdl.h
+++ Tetris/sdl.h
@@ -1,34 +1,41 @@
+//main library
+#include "SDL/include/SDL.h"	
+#pragma comment (lib, "SDL/lib/SDL.lib")
+
+//fill boxes
+#include "SDL/include/SDL_gfxPrimitives.h" 
+#pragma comment (lib, "SDL/lib/SDL_GfxPrimitives_Static.lib") 
+
+//fonts
+#include "SDL/include/SDL_ttf.h"
+#pragma comment (lib, "SDL/lib/SDL_ttf.lib")
+
+//sounds
+#include "SDL/include/SDL_mixer.h"
+#pragma comment (lib, "SDL/lib/SDL_mixer")
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 16;
+enum color {BLACK, WHITE, RED, GREEN, BLUE, YELLOW, DARKBLUE, COLOR_MAX};
+
 #ifndef _SDL_
 #define _SDL_
-
-#ifndef LINUX
-#include "SDL/include/SDL.h"
-#include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
-
-#else
-
-#include <SDL/SDL.h>
-#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
-
-#endif
-
-#pragma comment (lib, "SDL/lib/SDL.lib")
-#pragma comment (lib,"SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
-
-enum color {BLACK,RED,GREEN,BLUE,CYAN,MAGENTA,YELLOW,WHITE,COLOR_MAX};
 
 class SDL
 {
 public:
-	SDL();
 
-	void DrawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC);
-	void ClearScreen();
-	int GetScreenHeight();
-	int InitGraph();
-	int Pollkey();
-	int Getkey();
-	int IsKeyDown(int pKey);
-	void UpdateScreen();
+	SDL						();
+
+	void DrawRectangle		(int pX1, int pY1, int pX2, int pY2, enum color pC);
+	void ClearScreen		();
+	int GetScreenHeight		();
+	int InitGraph			();
+	int Pollkey				();
+	int Getkey				();
+	int IsKeyDown			(int pKey);
+	void UpdateScreen		();
 };
+
 #endif
