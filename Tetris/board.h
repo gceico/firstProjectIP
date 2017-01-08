d55b4ea5bb9604fb9a7fead5e30299035b124107
LazarAlexandru <alexlazar84@yahoo.com>
2017-01-08 13:39:53 +0200

    update board.h
    am adaugat variabile+functii pentru scor

--- Tetris/board.h
+++ Tetris/board.h
@@ -1,5 +1,6 @@
+#ifndef  _BOARD_
+#define _BOARD_
 #include "pieces.h"
-
 #define BOARD_LINE_WIDTH 6
 #define BLOCK_SIZE 16
 #define BOARD_POSITION 320
@@ -8,22 +9,29 @@
 #define MIN_VERTICAL_MARGIN 20
 #define MIN_HORIZONTAL_MARGIN 20
 #define PICE_BLOCKS 5
+#define PIECE_BLOCKS 5
+#define TOP_SCORES 10
 
-class Board
-{
+class Board {
 public:
-	Board							(Pieces *pPieces, int pScreenHeight);
+	int finalScore = 0;
+	int highScore[TOP_SCORES];
+	void readScore();
+	void updateScore();
+	void updateFile();
 
-	int GetXPosInPixels				(int pPos);
-	int GetYPosInPixels				(int pPos);
-	bool IsFreeBlock				(int pX, int pY);
-	bool IsPossibleMovement			(int pX, int pY, int pPiece, int pRotation);
-	void StorePiece					(int pX, int pY, int pPiece, int pRotation);
-	void DeletePossibleLines		();
-	bool IsGameOver					();
+	Board(Pieces *pPieces, int pScreenHeight);
+
+	int GetXPosInPixels(int pPos);
+	int GetYPosInPixels(int pPos);
+	bool IsFreeBlock(int pX, int pY);
+	bool IsPossibleMovement(int pX, int pPiece, int pRotation);
+	void StorePiece(int pX, int pY, int pPiece, int pRotation);
+	void DeletePossibleLines();
+	bool IsGameOver();
 
 private:
-	enum {POS_FREE, POS_FILLED};
+	enum { POS_FREE, POS_FILLED };
 	int mBoard[BOARD_WIDTH][BOARD_HEIGHT];
 	Pieces *mPieces;
 	int mScreenHeight;
@@ -31,3 +39,5 @@ private:
 	void InitBoard();
 	void DeleteLine(int pY);
 };
+
+#endif 
