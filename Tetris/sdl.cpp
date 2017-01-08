74b03d1c525cbe92d0a7e5fc4ab213d4e859ae35
gceico <ceicoschi.gabriel@gmail.com>
2017-01-08 19:10:55 +0200

    Grafica
    Am adaugat resusrsele necesare pentru grafica si scor
    Am creat suprafetele pentru BG, scor si highScore, am definit fontul
    folosit pt scor si am creat functia de aplicare a suprafetiei urmat de
    modificarile necesare din InitGraph

--- Tetris/sdl.cpp
+++ Tetris/sdl.cpp
@@ -1,32 +1,74 @@
 #include "SDL.h"
+#include <string>
+using namespace std;
 
 static SDL_Surface *mScreen;
-static Uint32 mColors [COLOR_MAX]= { 0x000000ff,0xff0000ff, 0x00ff00ff, 0x0000ffff,0x00ffffff, 0xff00ffff, 0xffff00ff,0xffffffff };
+static Uint32 mColors[COLOR_MAX] = { 0x000000ff, 0xffffffff, 0xed2d0eff, 0x0fe00fff, 0x097bdcff, 0xd1d80fff, 0x090431ff };
 
-SDL::SDL() {
+//Aditional surfaces
+SDL_Surface *background = NULL;
+SDL_Surface *scoreS = NULL;
+SDL_Surface	*highScoreS = NULL;
+
+//Font
+TTF_Font *font = NULL;
+SDL_Color textColor = { 0, 0, 0 };
+
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination)
+{
+	SDL_Rect offset;
+
+	offset.x = x;
+	offset.y = y;
+
+	SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
+//init
+SDL::SDL()
+{
 	InitGraph();
 }
 
-void SDL::ClearScreen() {
-	boxColor(mScreen, 0, 0, mScreen->w - 1, mScreen->h - 1, mColors[BLACK]);
+
+
+//Clear the screen
+void SDL::ClearScreen()
+{
+	apply_surface(0, 0, background, mScreen);
 }
 
-void SDL::DrawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC) {
+
+//Draw a rectangle of a given color
+void SDL::DrawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC)
+{
 	boxColor(mScreen, pX1, pY1, pX2, pY2 - 1, mColors[pC]);
 }
 
-int SDL::GetScreenHeight() {
+//Return the screen height
+int SDL::GetScreenHeight()
+{
 	return mScreen->h;
 }
 
-void SDL::UpdateScreen() {
+
+
+//Update screen
+void SDL::UpdateScreen()
+{
 	SDL_Flip(mScreen);
 }
 
-int SDL::Pollkey() {
+
+
+//Keyboard Input
+int SDL::Pollkey()
+{
 	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		switch (event.type) {
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
 		case SDL_KEYDOWN:
 			return event.key.keysym.sym;
 		case SDL_QUIT:
@@ -36,9 +78,11 @@ int SDL::Pollkey() {
 	return -1;
 }
 
-int SDL::Getkey() {
+int SDL::Getkey()
+{
 	SDL_Event event;
-	while (true) {
+	while (true)
+	{
 		SDL_WaitEvent(&event);
 		if (event.type == SDL_KEYDOWN)
 			break;
@@ -48,7 +92,8 @@ int SDL::Getkey() {
 	return event.key.keysym.sym;
 }
 
-int SDL::IsKeyDown(int pKey) {
+int SDL::IsKeyDown(int pKey)
+{
 	Uint8* mKeytable;
 	int mNumkeys;
 	SDL_PumpEvents();
@@ -56,26 +101,44 @@ int SDL::IsKeyDown(int pKey) {
 	return mKeytable[pKey];
 }
 
-int SDL::InitGraph() {
-	const SDL_VideoInfo *info;
-	Uint8 video_bpp;
-	Uint32 videoflags;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		fprintf(stderr, "Couldn't initializate SDL: %s\n", SDL_GetError());
+
+//SDL Graphical Initialization
+int SDL::InitGraph()
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
 		return 1;
 	}
-	atexit(SDL_Quit);
-	info = SDL_GetVideoInfo();
-	if (info->vfmt->BitsPerPixel > 8) {
-		video_bpp = info->vfmt->BitsPerPixel;
+
+	if (TTF_Init() < 0)
+	{
+		return 1;
 	}
-	else {
-		video_bpp = 16;
-	}
-	videoflags = SDL_SWSURFACE | SDL_DOUBLEBUF;
-	if ((mScreen = SDL_SetVideoMode(640, 480, video_bpp, videoflags)) == NULL) {
-		fprintf(stderr, "Couldn't set %ix%i video mode: %s\n", 640, 480, SDL_GetError());
+	SDL_WM_SetCaption("Tetris", NULL);
+
+	if ((mScreen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE)) == NULL)
+	{
 		return 2;
 	}
+
+	background = SDL_LoadBMP("Resources/background.bmp");
+	if (background == NULL)
+		return 2;
+	else
+		apply_surface(0, 0, background, mScreen);
+
+	font = TTF_OpenFont("Resources/font.ttf", 3);
+	if (font == NULL)
+	{
+		return 2;
+	}
+
+	scoreS = TTF_RenderText_Solid(font, "ACEST FONT", textColor);
+
+	if (scoreS == NULL)
+		return 2;
+	else
+		apply_surface(0, 0, scoreS, mScreen);
+
 	return 0;
 }
